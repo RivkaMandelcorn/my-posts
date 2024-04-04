d7c665a8b0d866287b912ebef6dcdf4af22bfbc3
Rivka Miriam Mandelcorn <p0037013@ness-tech.co.il>
2024-04-04 15:37:21 +0300

    add service writers

---
+++ posts/src/app/writers/writers.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WritersService {
+
+  constructor() { }
+
+  async getWriters():Promise<Array<Writers>> {
+    var d = await fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json());
+      return await d;
+  }
+
+}
+export interface Writers {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  adress: Address;
+  phone: string;
+  website: string;
+  company: Company;
+}
+interface Address {
+  street: string;
+  suit: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+interface Geo {
+  lat: string;
+  lng: string;
+}
+interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}

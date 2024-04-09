ba8342ccc910a430ec7df5e2585367f186780ecb
rivki <rivki@gmail.com>
2024-04-10 00:24:45 +0300

    fix

--- posts/src/app/components/login/login.service.ts
+++ posts/src/app/components/login/login.service.ts
@@ -1,14 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Writers } from '../writers/writers.service';
 import { User } from '../model/User.modle';
-import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor(private http : HttpClient) { }
+  constructor() { }
 
   async getUser(userId: number): Promise<User> {
     var data = await fetch('https://jsonplaceholder.typicode.com/users')
